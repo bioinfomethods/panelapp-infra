8abb656f8fb125e2cd94b6e9c6eda8e5ccdee2d0
Vahid Farshadfar <vahidfarshadfar@Vahids-MacBook-Pro.local>
2019-06-19 15:23:50 +0100

    changed it to function

--- run-ecs-task.py
+++ run-ecs-task.py
@@ -5,32 +5,40 @@
 import boto3
 from botocore.exceptions import ClientError
 
+def get_security_id(sec_group_names):
 
-security_group_ids = []
-ec2 = boto3.client('ec2')
-security_groups = ['panelapp-fargate-panelapp-dev','aurora-dev']
-
-for security_group in security_groups:
-	response = ec2.describe_security_groups(
+  """
+  This returns the security group ids by providing the group names
+  """
+  ec2 = boto3.client('ec2')
+  id_list=[]
+  for security_group_name in sec_group_names:
+    response = ec2.describe_security_groups(
     Filters=[
-        dict(Name='group-name', Values=[security_group])
-    ]
-)
-	security_group_ids.append(response['SecurityGroups'][0]['GroupId'])
+        dict(Name='group-name', Values=[security_group_name])
+      ]
+      )
+    id_list.append(response['SecurityGroups'][0]['GroupId'])
+
+  return id_list
 
 
-tasks=['panelapp-migrate-panelapp-dev','panelapp-collectstatic-panelapp-dev','panelapp-loaddata-panelapp-dev','panelapp-createsuperuser-panelapp-dev']
-ecs = boto3.client('ecs')
 
-for task in tasks:
-  try:
-    run_task = ecs.run_task(
-  		cluster='panelapp-cluster-dev',
-  		launchType = 'FARGATE',
-  		taskDefinition=task,
-  		count = 1,
-  		platformVersion='LATEST',
-  		networkConfiguration={
+def run_fargate_task(tasks, security_group_ids):
+
+  """
+  This will run a list of tasks with provided security group ids list attached to it
+  """
+  ecs = boto3.client('ecs')
+  for task in tasks:
+    try:
+      run_task = ecs.run_task(
+      cluster='panelapp-cluster-dev',
+      launchType = 'FARGATE',
+      taskDefinition=task,
+      count = 1,
+      platformVersion='LATEST',
+      networkConfiguration={
         'awsvpcConfiguration': {
             'subnets': [
                 'subnet-05d4f3d7e97901d41',
@@ -38,7 +46,12 @@ for task in tasks:
             ],
             'securityGroups': security_group_ids,
             'assignPublicIp': 'ENABLED'
-        	}
-    	})
-  except ClientError as e:
-  	print (e)
+          }
+      })
+    except ClientError as e:
+      print (e)
+
+panelapp_security_groups = ['panelapp-fargate-panelapp-dev','aurora-dev']
+panelapp_tasks = ['panelapp-migrate-panelapp-dev','panelapp-collectstatic-panelapp-dev','panelapp-loaddata-panelapp-dev','panelapp-createsuperuser-panelapp-dev']
+panelapp_security_group_ids = get_security_id(panelapp_security_groups)
+run_fargate_task(panelapp_tasks,panelapp_security_group_ids)
