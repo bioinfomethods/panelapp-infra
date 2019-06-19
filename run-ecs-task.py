804cf3d4150b81e8a47a4012c50547f77fbe4cf6
Vahid Farshadfar <vahidfarshadfar@Vahids-MacBook-Pro.local>
2019-06-19 17:27:44 +0100

    pass arguments

--- run-ecs-task.py
+++ run-ecs-task.py
@@ -2,8 +2,22 @@
 
 # This is just an example to start one off task, has to be improved
 
+import argparse
 import boto3
 from botocore.exceptions import ClientError
+
+parser = argparse.ArgumentParser(description='Perform fargate tasks')
+
+parser.add_argument('-sgs', help='name of the security groups with a space in between', nargs='+')
+
+parser.add_argument('-tasks', help='name of the tasks with a space in between', nargs='+')
+
+parser.add_argument('-cluster', help='name of the cluster to run the task', type=str)
+
+parser.add_argument('-subnets', help='subnets ids to run the tasks with a space in between', nargs='+')
+
+args = parser.parse_args()
+
 
 def get_security_id(sec_group_names):
 
@@ -24,7 +38,7 @@ def get_security_id(sec_group_names):
 
 
 
-def run_fargate_task(tasks, security_group_ids, cluster):
+def run_fargate_task(tasks, security_group_ids, cluster, subnets):
 
   """
   This will run a list of tasks with provided security group ids list attached to it in a given cluster
@@ -40,10 +54,7 @@ def run_fargate_task(tasks, security_group_ids, cluster):
       platformVersion='LATEST',
       networkConfiguration={
         'awsvpcConfiguration': {
-            'subnets': [
-                'subnet-05d4f3d7e97901d41',
-                'subnet-0c4fe09b0863c883e'
-            ],
+            'subnets': subnets,
             'securityGroups': security_group_ids,
             'assignPublicIp': 'ENABLED'
           }
@@ -51,7 +62,10 @@ def run_fargate_task(tasks, security_group_ids, cluster):
     except ClientError as e:
       print (e)
 
-panelapp_security_groups = ['panelapp-fargate-panelapp-dev','aurora-dev']
-panelapp_tasks = ['panelapp-migrate-panelapp-dev','panelapp-collectstatic-panelapp-dev','panelapp-loaddata-panelapp-dev','panelapp-createsuperuser-panelapp-dev']
-panelapp_security_group_ids = get_security_id(panelapp_security_groups)
-run_fargate_task(panelapp_tasks,panelapp_security_group_ids,'panelapp-cluster-dev')
+# panelapp_security_groups = ['panelapp-fargate-panelapp-dev','aurora-dev']
+# panelapp_tasks = ['panelapp-migrate-panelapp-dev','panelapp-collectstatic-panelapp-dev','panelapp-loaddata-panelapp-dev','panelapp-createsuperuser-panelapp-dev']
+
+
+panelapp_security_groups = args.sgs
+panelapp_security_group_ids = get_security_id(args.sgs)
+run_fargate_task(args.tasks, panelapp_security_group_ids, args.cluster, args.subnets)
