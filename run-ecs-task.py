3df53ff08138f710fb0da53b055fb91cd5e588fa
Vahid Farshadfar <vahidfarshadfar@Vahids-MacBook-Pro.local>
2019-06-20 16:40:01 +0100

    added some error handeling

--- run-ecs-task.py
+++ run-ecs-task.py
@@ -2,19 +2,21 @@
 
 # This is just an example to start one off task, has to be improved
 
+import os
+import sys
 import argparse
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, ParamValidationError
 
 parser = argparse.ArgumentParser(description='Perform fargate tasks')
 
-parser.add_argument('-sgs', help='name of the security groups with a space in between', nargs='+')
+parser.add_argument('-sgs', help='name of the security groups with a space in between', nargs='+', default=os.environ.get(''))
 
-parser.add_argument('-tasks', help='name of the tasks with a space in between', nargs='+')
+parser.add_argument('-tasks', help='name of the tasks with a space in between', nargs='+', default=os.environ.get(''))
 
-parser.add_argument('-cluster', help='name of the cluster to run the task', type=str)
+parser.add_argument('-cluster', help='name of the cluster to run the task', type=str, default=os.environ.get(''))
 
-parser.add_argument('-subnets', help='subnets ids to run the tasks with a space in between', nargs='+')
+parser.add_argument('-subnets', help='subnets ids to run the tasks with a space in between', nargs='+', default=os.environ.get(''))
 
 args = parser.parse_args()
 
@@ -59,13 +61,20 @@ def run_fargate_task(tasks, security_group_ids, cluster, subnets):
             'assignPublicIp': 'ENABLED'
           }
       })
-    except ClientError as e:
-      print (e)
 
+      print("\nTask " '"{}"' " Has been initiated in " '"{}"' " cluster\n".format(task,cluster))
+
+    except (ClientError,ParamValidationError) as e:
+      print("\nTask " '"{}"' " initiation failed in " '"{}"' " cluster with the following error: ".format(task,cluster)),print( '{}' '\n'.format(e))
+    
 # panelapp_security_groups = ['panelapp-fargate-panelapp-dev','aurora-dev']
 # panelapp_tasks = ['panelapp-migrate-panelapp-dev','panelapp-collectstatic-panelapp-dev','panelapp-loaddata-panelapp-dev','panelapp-createsuperuser-panelapp-dev']
 
+try:
+  panelapp_security_groups = args.sgs
+  panelapp_security_group_ids = get_security_id(args.sgs)
+  run_fargate_task(args.tasks, panelapp_security_group_ids, args.cluster, args.subnets)
+except (IndexError, TypeError) as error:
+  print('\nIt is likely that one or some of the arguments has not been defined or provided incorrectly, see the error and the usage below\n'),print(error),parser.print_help()
 
-panelapp_security_groups = args.sgs
-panelapp_security_group_ids = get_security_id(args.sgs)
-run_fargate_task(args.tasks, panelapp_security_group_ids, args.cluster, args.subnets)
+
