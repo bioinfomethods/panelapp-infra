84b1e162fe2f25d7788933d45a4eeab976f4ac7b
bevinsonbinu <bevinson.binu@genomicsengland.co.uk>
2019-06-26 23:28:22 +0100

    IP-3394-Add-GitlabRunner-config - Required terraform scripts added

---
+++ terraform/infra/gitlab_runners.tf
@@ -0,0 +1,14 @@
+module "gitlab-runners" {
+  source = "../modules/gitlab-runners"
+
+  stack        = "${var.stack}"
+  vpc_id       = "${module.vpc.vpc_id}"
+  ecs_subnets  = "${module.vpc.private_subnets}"
+  site_kms_key = "*"                  # FIXME"${module.site.site_key}"
+  default_tags = "${var.default_tags}"
+  token_from = "arn:aws:ssm:${var.region}:${var.account_id}:parameter/${var.stack}/${var.env_name}/REGISTRATION_TOKEN"
+
+  app_image = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/gitlab-runner:${var.gitlab_runner_image_tag}"
+
+  create_runner_terraform = "${var.create_runner_terraform}"
+}
---
+++ terraform/modules/gitlab-runners/main.tf
@@ -0,0 +1,21 @@
+resource "aws_ecs_cluster" "gitlab_runners" {
+  count = "${var.create_gitlab_runners ? 1 : 0}"
+  name  = "gitlab-runners"
+}
+
+resource "aws_cloudwatch_log_group" "gitlab_runners" {
+  count             = "${var.create_gitlab_runners ? 1 : 0}"
+  name              = "gitlab-runners"
+  retention_in_days = 3
+}
+
+data "template_file" "gitlab_runner" {
+  template = "${file("${path.module}/gitlab_runner.tpl")}"
+
+  vars {
+    app_cpu    = "${var.app_cpu}"
+    app_image  = "${var.app_image}"
+    app_memory = "${var.app_memory}"
+    token_from = "${var.token_from}"
+  }
+}
