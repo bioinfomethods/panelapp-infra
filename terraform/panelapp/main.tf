5ed5a213d69f3c2583286acd32d770e97fd31bd6
Vahid Farshadfar <vahidfarshadfar@Vahids-MacBook-Pro.local>
2019-07-16 15:46:07 +0100

    cloudflare to use env var

--- terraform/panelapp/main.tf
+++ terraform/panelapp/main.tf
@@ -15,8 +15,6 @@ provider "template" {
 
 provider "cloudflare" {
   version = "~> 1.16"
-  email   = "${var.cloudflare_email}"
-  token   = "${var.cloudflare_token}"
 }
 
 // State of `infra` component
