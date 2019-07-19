6761480534e29b77f267279c08d1a9d78eb81ffd
Michal Stypik <michal.stypik@contino.io>
2019-07-19 15:07:20 +0100

    add datadog

---
+++ terraform/modules/site/datadog.tf
@@ -0,0 +1,65 @@
+resource "aws_iam_role" "datadog_aws_integration" {
+  name               = "datadog-integration"
+  description        = "Role for Datadog AWS Integration"
+  assume_role_policy = "${data.aws_iam_policy_document.datadog_aws_integration_assume_role.json}"
+}
+
+resource "aws_iam_policy" "datadog_aws_integration" {
+  name   = "${var.stack}-datadog-integration"
+  policy = "${data.aws_iam_policy_document.datadog.json}"
+}
+
+data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::464622532012:root"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+
+      values = [
+        "${var.datadog_aws_integration_external_id}",
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "datadog" {
+  statement {
+    sid = "1"
+
+    actions = [
+        "autoscaling:Describe*",
+        "cloudwatch:Describe*",
+        "cloudwatch:Get*",
+        "cloudwatch:List*",
+        "ec2:Describe*",
+        "ecs:Describe*",
+        "ecs:List*",
+        "elasticloadbalancing:Describe*",
+        "logs:Get*",
+        "logs:Describe*",
+        "logs:FilterLogEvents",
+        "logs:TestMetricFilter",
+        "logs:PutSubscriptionFilter",
+        "logs:DeleteSubscriptionFilter",
+        "logs:DescribeSubscriptionFilters",
+        "rds:Describe*",
+        "rds:List*",
+        "ses:Get*",
+        "sqs:ListQueues",
+        "tag:GetResources",
+        "tag:GetTagKeys",
+        "tag:GetTagValues",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
