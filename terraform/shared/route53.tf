10fd8ab96ad7265ae159129dcb7137d1029c13d3
bevinsonbinu <bevinson.binu@genomicsengland.co.uk>
2019-10-03 11:18:14 +0100

    CM-1754-Update-DNS-Route53-zone-Part 1
    Updated variabled and zone id

--- terraform/shared/route53.tf
+++ terraform/shared/route53.tf
@@ -16,12 +16,7 @@ resource "aws_route53_record" "test" {
   type            = "NS"
   zone_id         = "${aws_route53_zone.public.zone_id}"
 
-  records = [
-    "ns-1932.awsdns-49.co.uk",
-    "ns-1301.awsdns-34.org",
-    "ns-563.awsdns-06.net",
-    "ns-113.awsdns-14.com"
-  ]
+  records         = "${var.test_panelapp_ns_records}"
 }
 
 resource "aws_route53_record" "stage" {
@@ -31,12 +26,7 @@ resource "aws_route53_record" "stage" {
   type            = "NS"
   zone_id         = "${aws_route53_zone.public.zone_id}"
 
-  records = [
-    "ns-1214.awsdns-23.org",
-    "ns-1828.awsdns-36.co.uk",
-    "ns-427.awsdns-53.com",
-    "ns-740.awsdns-28.net"
-  ]
+  records         = "${var.stage_panelapp_ns_records}"
 }
 
 resource "aws_route53_record" "prod" {
@@ -46,12 +36,7 @@ resource "aws_route53_record" "prod" {
   type            = "NS"
   zone_id         = "${aws_route53_zone.public.zone_id}"
 
-  records = [
-    "ns-1025.awsdns-00.org",
-    "ns-1633.awsdns-12.co.uk",
-    "ns-506.awsdns-63.com",
-    "ns-657.awsdns-18.net"
-  ]
+  records         = "${var.prod_panelapp_ns_records}"
 }
 
 ################
@@ -60,53 +45,35 @@ resource "aws_route53_record" "prod" {
 
 
 data "aws_route53_zone" "route53_public" {
-  name         = "${var.public_dns_zone_name}"
   zone_id      = "${var.public_route53_zone_id}"
 }
 
-resource "aws_route53_record" "test" {
+resource "aws_route53_record" "ns_test" {
   allow_overwrite = true
   name            = "test"
   ttl             = 30
   type            = "NS"
   zone_id         = "${data.aws_route53_zone.route53_public.zone_id}"
 
-  records = [
-    "ns-1932.awsdns-49.co.uk",
-    "ns-1301.awsdns-34.org",
-    "ns-563.awsdns-06.net",
-    "ns-113.awsdns-14.com"
-  ]
+  records         = "${var.test_panelapp_ns_records}"
 }
 
-resource "aws_route53_record" "stage" {
+resource "aws_route53_record" "ns_stage" {
   allow_overwrite = true
   name            = "stage"
   ttl             = 30
   type            = "NS"
   zone_id         = "${data.aws_route53_zone.route53_public.zone_id}"
 
-  records = [
-    "ns-1214.awsdns-23.org",
-    "ns-1828.awsdns-36.co.uk",
-    "ns-427.awsdns-53.com",
-    "ns-740.awsdns-28.net"
-  ]
+  records         = "${var.stage_panelapp_ns_records}"
 }
 
-# Will be added as part-2 
+resource "aws_route53_record" "ns_prod" {
+  allow_overwrite = true
+  name            = "prod"
+  ttl             = 30
+  type            = "NS"
+  zone_id         = "${data.aws_route53_zone.route53_public.zone_id}"
 
-//resource "aws_route53_record" "prod" {
-//  allow_overwrite = true
-//  name            = "prod"
-//  ttl             = 30
-//  type            = "NS"
-//  zone_id         = "${data.aws_route53_zone.route53_public.zone_id}"
-//
-//  records = [
-//    "ns-1025.awsdns-00.org",
-//    "ns-1633.awsdns-12.co.uk",
-//    "ns-506.awsdns-63.com",
-//    "ns-657.awsdns-18.net"
-//  ]
-//}
+  records         = "${var.prod_panelapp_ns_records}"
+}
--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -19,5 +19,36 @@ variable "default_tags" {
 }
 
 variable "public_route53_zone_id" {
-  default = "Z3D0BLRM13ENHB"
+  default = "Z1SM5CM93IOIDU"
 }
+
+variable "test_panelapp_ns_records" {
+  type = "list"
+  default = [
+    "ns-1932.awsdns-49.co.uk",
+    "ns-1301.awsdns-34.org",
+    "ns-563.awsdns-06.net",
+    "ns-113.awsdns-14.com"
+  ]
+}
+
+variable "prod_panelapp_ns_records" {
+  type = "list"
+  default = [
+    "ns-1025.awsdns-00.org",
+    "ns-1633.awsdns-12.co.uk",
+    "ns-506.awsdns-63.com",
+    "ns-657.awsdns-18.net"
+  ]
+}
+
+variable "stage_panelapp_ns_records" {
+  type = "list"
+  default = [
+    "ns-1214.awsdns-23.org",
+    "ns-1828.awsdns-36.co.uk",
+    "ns-427.awsdns-53.com",
+    "ns-740.awsdns-28.net"
+  ]
+}
+
