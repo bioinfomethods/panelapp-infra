d4783a176c595e2db622cd997764956e3bd24fb3
Tommy Li <tommy.li@firefire.co>
2024-07-22 10:39:02 +1000

    style: Formatting

--- terraform/modules/aurora/aurora.tf
+++ terraform/modules/aurora/aurora.tf
@@ -8,14 +8,12 @@ resource "aws_rds_cluster" "aurora_cluster" {
   storage_encrypted         = true
   db_subnet_group_name      = aws_db_subnet_group.aurora.name
   kms_key_id                = var.rds_db_kms_key
-  vpc_security_group_ids = concat([aws_security_group.aurora.id], var.additional_security_groups)
-
-  database_name           = var.database
-  master_username         = var.username
-  backup_retention_period = var.rds_backup_retention_period
-
-  master_password = join("", data.aws_ssm_parameter.root_password.*.value)
-  snapshot_identifier = var.restore_from_snapshot ? var.rds_snapshot : ""
+  vpc_security_group_ids    = concat([aws_security_group.aurora.id], var.additional_security_groups)
+  database_name             = var.database
+  master_username           = var.username
+  backup_retention_period   = var.rds_backup_retention_period
+  master_password           = join("", data.aws_ssm_parameter.root_password.*.value)
+  snapshot_identifier       = var.restore_from_snapshot ? var.rds_snapshot : ""
 
   tags = merge(
     var.default_tags,
@@ -36,7 +34,7 @@ resource "aws_rds_cluster_instance" "aurora_cluster_instance" {
   instance_class          = var.instance_class
   db_subnet_group_name    = aws_db_subnet_group.aurora.name
   monitoring_interval     = var.mon_interval
-  monitoring_role_arn = join("", aws_iam_role.rds_enhanced_monitoring.*.arn)
+  monitoring_role_arn     = join("", aws_iam_role.rds_enhanced_monitoring.*.arn)
   publicly_accessible     = false
   promotion_tier          = "0"
   db_parameter_group_name = aws_db_parameter_group.custom_instance_parameters.id
