c4c7268d7e934983a9d1c8e8bfc28efcb3e89c7d
Tommy Li <tommy.li@firefire.co>
2024-07-31 18:03:46 +1000

    fix: More policies granted to github-oidc client

--- terraform/infra/github.tf
+++ terraform/infra/github.tf
@@ -6,8 +6,20 @@ module "github-oidc" {
   create_oidc_role     = true
 
   repositories = ["bioinfomethods/panelapp", "bioinfomethods/panelapp-infra"]
+
+  # TODO - Review the policies attached to this OIDC role intended for DevOps managed using Terraform
+  # Whilst custom policies is arguably a better practice (principle of least privileged), the following is easier to
+  # manage since these policies are managed by AWS.
   oidc_role_attach_policies = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser",
     "arn:aws:iam::aws:policy/AmazonECS_FullAccess",
+    "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess",
+    "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess",
+    "arn:aws:iam::aws:policy/AmazonSQSReadOnlyAccess",
+    "arn:aws:iam::aws:policy/AmazonCognitoReadOnly",
+    "arn:aws:iam::aws:policy/ElasticLoadBalancingReadOnly",
+    "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
   ]
 }
