83e37d37097145de146e276223c4cb72d5f00105
chris <chris.ieng@mcri.edu.au>
2025-12-03 12:31:22 +1100

    remove unused SQS policy to allow for <= 10 policy attachment limit

--- terraform/infra/github.tf
+++ terraform/infra/github.tf
@@ -17,7 +17,6 @@ module "github-oidc" {
     "arn:aws:iam::aws:policy/AmazonECS_FullAccess",
     "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess",
     "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess",
-    "arn:aws:iam::aws:policy/AmazonSQSReadOnlyAccess",
     "arn:aws:iam::aws:policy/AmazonCognitoReadOnly",
     "arn:aws:iam::aws:policy/CloudFrontFullAccess",
     "arn:aws:iam::aws:policy/ElasticLoadBalancingReadOnly",
